9033445bd005ed7bb4fe532105937efe0069fc3b
cawilson1 <cawilson1@g.cofc.edu>
2020-05-20 18:14:01 -0400

    added initial routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Router } from "@reach/router";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+const Recipes = props => {
+  return <div>{props.children}</div>;
+};
+const RecipeIndex = () => {
+  return <div>Recipe Index</div>;
+};
+const Recipe = props => {
+  return <div>Recipe {props.recipeId}</div>;
+};
+const NotFound = () => {
+  return <div>NotFound</div>;
+};
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Home path="/" />
+        <Recipes path="recipes">
+          <Recipe path=":recipeId" />
+          <RecipeIndex path="/" />
+        </Recipes>
+        <NotFound default />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
