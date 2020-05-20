0b925f201bc2797ba4cf612ebdc17797e6d25d16
cawilson1 <cawilson1@g.cofc.edu>
2020-05-20 18:26:34 -0400

    use useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Router, Link } from "@reach/router";
 import recipesdb from "./recipesdb";
 import { Card } from "@material-ui/core";
@@ -10,9 +10,14 @@ const Recipes = props => {
   return <div>{props.children}</div>;
 };
 const RecipeIndex = () => {
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    //do a real request to an API/ server/db. Async fn usually
+    setRecipes(recipesdb);
+  }, []);
   return (
     <div>
-      {recipesdb.map(recipe => {
+      {recipes.map(recipe => {
         return (
           <Card style={{ margin: 20, padding: 10 }}>
             <div>{recipe.name}</div>
@@ -29,7 +34,7 @@ const Recipe = props => {
   return <div>Recipe {props.recipeId}</div>;
 };
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return <div>Sorry, there is no page at this location!</div>;
 };
 
 function App() {
